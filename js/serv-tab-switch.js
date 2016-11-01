faebd701f2c80c8175dc196fa4b4bcb58d8f0f94
zhanwen <604748955@qq.com>
2016-11-01 15:41:56 +0800

    11-1修改2page

--- js/serv-tab-switch.js
+++ js/serv-tab-switch.js
@@ -2,6 +2,7 @@
 		var isEv = /Mobile/.test(window.navigator.userAgent);
     	var Ev = isEv?"touchstart":"click";
     	$(".serv-item-title>span").on(Ev,function(){
+    		$(this).addClass("serv-selected").siblings().removeClass("serv-selected");
     		var title_num=$(this).index()+1;var a;
     		$(".serv-content"+title_num).removeClass("dis").siblings().addClass("dis");
     	});
